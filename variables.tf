cfb13e99d6fb507447611f4258c394336d165101
Pablo Ponce <47292039+pablosponce@users.noreply.github.com>
2022-05-22 12:14:06 +0200

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -2,13 +2,3 @@ variable "aws_region" {
  type=string
  default="eu-north-1"
 }
-
-variable "aws_key" {
- type=string
- default=${{ secrets.AWS_KEY }}
-}
-
-variable "aws_secret" {
- type=string
- default=${{ secrets.AWS_SECRET }}
-}
