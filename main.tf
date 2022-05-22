48b3839914333b1f4bca42e2f0d11fd065a8d584
Pablo Ponce <47292039+pablosponce@users.noreply.github.com>
2022-05-22 12:19:38 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,14 +1,14 @@
 terraform {
-       backend "remote" {
-         # The name of your Terraform Cloud organization.
-         organization = "momoconsulting"
+  backend "remote" {
+    # The name of your Terraform Cloud organization.
+    organization = "momoconsulting"
 
-         # The name of the Terraform Cloud workspace to store Terraform state files in.
-         workspaces {
-           name = "tf-githubactions"
-         }
-       }
-     }
+    # The name of the Terraform Cloud workspace to store Terraform state files in.
+    workspaces {
+      name = "tf-githubactions"
+    }
+  }
+}
 
 
 resource "aws_s3_bucket" "c" {
@@ -16,8 +16,8 @@ resource "aws_s3_bucket" "c" {
 }
 
 resource "aws_s3_object" "object" {
-  bucket = aws_s3_bucket.c.bucket
-  key    = "index.html"
+  bucket  = aws_s3_bucket.c.bucket
+  key     = "index.html"
   content = "Hola!!!"
 }
 
